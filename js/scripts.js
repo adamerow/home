9e4df02cf12415cbf7d5c09521b0fd9686eb0801
adamerow <adamerow@vcd-chn204-11.eastern.ewu.edu>
2016-10-27 14:51:53 -0700

    updating info & beginning layout

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,14 @@
-//MUST USE LETTERING PLUGIN
-//MUST USE A PLUGIN FROM UNHEAP
+// Intialize Lettering JS
+$('h1').lettering();
+
+// Intialize Backstretch
+$('.left').backstretch('/nameplate/img/bg.jpg');
+
+// Intialize Masonry
+$('.right').masonry({
+	itemSelector: 'section',
+	// MUST MATCH CSS
+    columnWidth: 255,
+	gutter: 10,
+	isFitWidth: true
+});
