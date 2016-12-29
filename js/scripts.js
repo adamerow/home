3b349db73444ae21d3bab573ee68c40251b49947
adamerow <abbydamerow@gmail.com>
2016-12-28 23:19:05 -0800

    various
    -fixed "work" problem
    -added media query
    -jquery media query problem

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,77 @@
 $('h1').lettering();
 
 // Intialize Backstretch
-$('.left').backstretch([
-      "img/me.jpg"
+$('.left').backstretch(
+[
+    "img/me.jpg"
     , "img/orange.png"
     , "img/quadcopter.png"
-  ], {duration: 4000, fade: 750});
+], {duration: 5500, fade: 800});
 
 // Intialize Masonry
 $('.masonry').masonry({
 	itemSelector: 'section',
 	// MUST MATCH CSS
-    columnWidth: 293,
+    columnWidth: 288,
 	gutter: 10,
 	isFitWidth: true
+});
+
+//Startup Layout
+$('#contact').addClass('closed');
+$('#contact .close').hide();
+
+$('#resume').addClass('closed');
+$('#resume .close').hide();
+
+//Close Buttons (closed)
+
+$('#contact .close').click( function()
+{
+    console.log("close clicked");
+    $('#contact').removeClass('open', 'animate');
+    $('#contact').addClass('closed');
+//    $('#contact').animate(
+//        { height: 63 }, 200);
+    $('#contact .close').hide();
+});
+
+$('#resume .close').click( function()
+{
+    console.log("close clicked");
+    $('#resume').removeClass('open', 'animate');
+    $('#resume').addClass('closed');
+//    $('#resume').animate(
+//        { height: 63 }, 200);
+    $('#resume .close').hide();
+});
+
+//Close Buttons (open)
+$('#contact h2').click( function()
+{
+    console.log("open clicked");
+    $('#contact').removeClass('closed');
+    $('#contact').removeClass('animate');
+    $('#contact .close').show();
+});
+
+$('#resume h2').click( function()
+{
+    console.log("open clicked");
+    $('#resume').removeClass('closed', 'animate');
+    $('#resume').addClass('open');
+    $('#resume .close').show();
+});
+
+//MEDIA QUERY
+$(window).resize(function ()
+{
+    var viewportWidth = $(window).width();
+    if (viewportWidth < 768)
+    {
+        console.log("open clicked");
+        $('#contact').removeClass('closed');
+        $('#contact').removeClass('animate');
+        $('#contact .close').show();
+    }
 });
